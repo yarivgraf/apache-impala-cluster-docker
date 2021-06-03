661f2eeace00ce0ba192d54ab4e93ee33e204870
Yariv Graf <yariv.graf@adk2.com>
2021-06-03 18:07:38 +0300

    Add im-dep

--- imp-dep/startup/impalad.sh
+++ imp-dep/startup/impalad.sh
@@ -7,7 +7,7 @@ echo never > /sys/kernel/mm/transparent_hugepage/defrag
 apt update
 apt -y install docker.io
 NAMENODE=`curl -s http://metadata.google.internal/computeMetadata/v1/instance/attributes/namenode -H 'Metadata-Flavor: Google'`
-NAMENODE_IP=`host $NAMENODE| awk  '{ print $4 }'`
+NAMENODE_IP=`host $NAMENODE | gawk '{ print $4 }'`
 cd ~
 git clone https://github.com/yarivgraf/apache-impala-cluster-docker.git
 
@@ -21,13 +21,12 @@ systemctl start data0.mount data1.mount data2.mount data3.mount data4.mount
 #### DATANODE
 cd ~/apache-impala-cluster-docker/imp-dep/apache-hadoop-3.2.2/
 sed -i -- "s/changeme/$NAMENODE/g" *
-docker run --net=host --name namenode -v ~/apache-impala-cluster-docker/imp-dep/apache-hadoop-3.2.2:/opt/hadoop/conf -v /var/lib/hadoop-hdfs/cache/hdfs/dfs/name:/var/lib/hadoop-hdfs/cache/hdfs/dfs/name --restart always -d yarivgraf/apache-hadoop-3.2.2:latest /run-namenode.sh
-
 docker run --net=host --name datanode --restart always -v ~/apache-impala-cluster-docker/imp-dep/apache-hadoop-3.2.2:/opt/hadoop/conf -v /var/run/hadoop-hdfs:/var/run/hadoop-hdfs -v /data0:/data0 -v /data1:/data1 -v /data2:/data2 -v /data3:/data3 -v /data4:/data4 -d yarivgraf/apache-hadoop-3.2.2:latest /run-datanode.sh
 
 
 #### IMPALAD
 cd ~/apache-impala-cluster-docker/imp-dep/apache-impala-3.4.0/conf
 sed -i -- "s/changeme/$NAMENODE/g" *
-docker run --net=host --name impalad -v ~/apache-impala-cluster-docker/imp-dep/apache-impala-3.4.0/conf:/opt/impala/conf -e IP=$NAMENODE_IP --restart always -v /var/run/hadoop-hdfs:/var/run/hadoop-hdfs -v /opt/impala/logs:/opt/impala/logs -d yarivgraf/apache-impala-3.4.0:latest  /entrypoint_impalad.sh
+docker run --net=host --name impalad -v ~/apache-impala-cluster-docker/imp-dep/apache-impala-3.4.0/conf:/opt/impala/conf -e IP=$NAMENODE_IP --restart always -v /var/run/hadoop-hdfs:/var/run/hadoop-hdfs -v /opt/impala/logs:/opt/impala/logs -d yarivgraf/apache-impala-3.4.0:latest /entrypoint_impalad.sh
+
 
